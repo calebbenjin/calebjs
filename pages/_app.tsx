e616e0bb6d90df243c5c09be71338e1f8e50a83c
calebbenjin <calebben94@gmail.com>
2022-12-21 23:29:04 +0100

    images and projectsCard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
+
+import { ThemeProvider } from 'next-themes'
+import { Inter } from '@next/font/google';
 import type { AppProps } from 'next/app'
 
+const interVariable = Inter();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider attribute="class">
+      <main className={interVariable.className}>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-import Head from 'next/head'
+import { Suspense } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import Head from 'next/head'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
+import BlogPostCard from '../components/BlogPostCard'
+import ProjectCard from '../components/ProjectCard'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +18,129 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        
-      </main>
+      <Suspense fallback={null}>
+        <Layout>
+          <div className="flex flex-col justify-center items-start max-w-4xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
+            <div className="flex flex-col-reverse sm:flex-row items-start">
+              <div className="flex flex-col pr-8">
+                <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
+                  Caleb Benjamin
+                </h1>
+                <h2 className="text-gray-700 text-2xl dark:text-gray-200 mb-4 mt-4">
+                  Frontend Developer / Mentor at{' '}
+                  <span className="font-semibold">Stutern</span>
+                </h2>
+                <p className="text-gray-600 dark:text-gray-400 mb-16">
+                  Helping developers build a faster web. Teaching about web
+                  development, serverless, and React / Next.js.
+                </p>
+              </div>
+              <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">
+                <Image
+                  alt="Caleb Benjamin"
+                  height={176}
+                  width={176}
+                  src="/avatar.jpg"
+                  sizes="30vw"
+                  priority
+                  className="rounded-full filter grayscale"
+                />
+              </div>
+            </div>
+
+            <h3 className="font-bold text-2xl md:text-4xl tracking-tight mt-10 mb-10 text-black dark:text-white">
+              Featured Projects
+            </h3>
+            <div className="flex gap-6 flex-col md:flex-row">
+              <ProjectCard
+                title="Eachblock - A creative design Agency website."
+                slug="style-guides-component-libraries-design-systems"
+                gradient="from-[#D8B4FE] to-[#818CF8]"
+                src="/projectimg.png"
+              />
+              <ProjectCard
+                title="CapitalRollup -  A crypto indexes web app that helps users invest in stable coing USDT"
+                slug="style-guides-component-libraries-design-systems"
+                gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
+                src="/projectimg2.png"
+              />
+              <ProjectCard
+                title="Everything I Know About Style Guides, Design Systems, and Component Libraries"
+                slug="style-guides-component-libraries-design-systems"
+                gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
+                src="/projectimg3.png"
+              />
+            </div>
+            <Link
+              href="/project"
+              className="flex items-center mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
+            >
+              <>
+                {'View all Projects'}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="h-6 w-6 ml-1"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+                  />
+                </svg>
+              </>
+            </Link>
+
+            <h3 className="font-bold text-2xl md:text-4xl tracking-tight mt-20 mb-10 mt-10 text-black dark:text-white">
+              Featured Posts
+            </h3>
+            <div className="flex gap-6 flex-col md:flex-row">
+              <BlogPostCard
+                title="Everything I Know About Style Guides, Design Systems, and Component Libraries"
+                slug="style-guides-component-libraries-design-systems"
+                gradient="from-[#D8B4FE] to-[#818CF8]"
+              />
+              <BlogPostCard
+                title="Rust Is The Future of JavaScript Infrastructure"
+                slug="rust"
+                gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
+              />
+              <BlogPostCard
+                title="Past, Present, and Future of React State Management"
+                slug="react-state-management"
+                gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
+              />
+            </div>
+            <Link
+              href="/blog"
+              className="flex items-center mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
+            >
+              <>
+                {'Read all posts'}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="h-6 w-6 ml-1"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+                  />
+                </svg>
+              </>
+            </Link>
+          </div>
+
+          
+        </Layout>
+      </Suspense>
     </>
   )
 }
